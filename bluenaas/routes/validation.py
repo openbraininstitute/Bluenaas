45beb4e7ec2f1b0c959dd63013ead97046962b0c
Bilal Meddah <bilal.meddah@epfl.ch>
2024-09-03 17:13:37 +0200

    update: add validation for formula endpoint

---
+++ bluenaas/routes/validation.py
@@ -0,0 +1,30 @@
+"""
+Synapse Placement Generation:
+Exposes an endpoint (`/generate-placement`) to generate synapse placements based on user-provided parameters
+"""
+
+from fastapi import APIRouter, Body, Depends, Request
+from bluenaas.domains.morphology import (
+    SynapsePlacementResponse,
+)
+from bluenaas.infrastructure.kc.auth import verify_jwt
+from bluenaas.services.validate_synapse_formula import (
+    validate_synapse_generation_formula,
+)
+
+
+router = APIRouter(prefix="/validation")
+
+
+@router.post(
+    "/synapse-formula",
+    response_model=bool,
+)
+def place_synapses(
+    request: Request,
+    formula: str = Body(embed=True),
+    token: str = Depends(verify_jwt),
+) -> SynapsePlacementResponse:
+    return validate_synapse_generation_formula(
+        formula=formula
+    )
