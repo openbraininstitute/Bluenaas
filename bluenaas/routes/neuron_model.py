54c137e68600c657485d58122c3d81eb72712ed5
Dinika Saxena <dinikasaxenas@gmail.com>
2024-11-18 16:20:59 +0100

    Add endpoints for retrieving neuron models

--- bluenaas/routes/neuron_model.py
+++ bluenaas/routes/neuron_model.py
@@ -1,7 +1,19 @@
 from datetime import datetime
-from typing import Optional
-from fastapi import APIRouter, Query
+from typing import Optional, Literal
+from fastapi import APIRouter, Query, Depends
 
+from bluenaas.domains.simulation import PaginatedResponse
+from bluenaas.infrastructure.kc.auth import verify_jwt
+from bluenaas.domains.neuron_model import (
+    SynaptomeModelResponse,
+    MEModelResponse,
+)
+from bluenaas.services.neuron_model.get_all_neuron_models_for_project import (
+    get_all_neuron_models_for_project,
+)
+from bluenaas.services.neuron_model.get_neuron_model_for_project import (
+    get_neuron_model_for_project,
+)
 
 router = APIRouter(
     prefix="/neuron-model",
@@ -10,78 +22,45 @@ router = APIRouter(
 
 
 @router.get(
-    "/{org_id}/{project_id}/me-models",
+    "/{org_id}/{project_id}",
     summary="Retrieve all me models for a specific project",
 )
-def retrieve_me_models(
+def retrieve_neuron_models(
     org_id: str,
     project_id: str,
-    page_offset: int = 0,
+    offset: int = 0,
     page_size: int = 20,
+    model_type: Optional[Literal["me-model", "synaptome"]] = None,
     created_at_start: Optional[datetime] = Query(
         None, description="Filter by createdAt date (YYYY-MM-DDTHH:MM:SSZ)"
     ),
     created_at_end: Optional[datetime] = Query(
         None, description="Filter by createdAt date (YYYY-MM-DDTHH:MM:SSZ)"
     ),
-):
-    pass
+    token: str = Depends(verify_jwt),
+) -> PaginatedResponse[MEModelResponse | SynaptomeModelResponse]:
+    return get_all_neuron_models_for_project(
+        token=token,
+        org_id=org_id,
+        project_id=project_id,
+        offset=offset,
+        size=page_size,
+        model_type=model_type,
+        created_at_start=created_at_start,
+        created_at_end=created_at_end,
+    )
 
 
 @router.get(
-    "/{org_id}/{project_id}/me-models/{model_id}",
+    "/{org_id}/{project_id}/{model_id:path}",
     summary="Retrieve specific model in a project",
 )
-def retrieve_me_model(
+def retrieve_neuron_model(
+    org_id: str,
+    project_id: str,
     model_id: str,
-    org_id: str,
-    project_id: str,
-    page_offset: int = 0,
-    page_size: int = 20,
-    created_at_start: Optional[datetime] = Query(
-        None, description="Filter by createdAt date (YYYY-MM-DDTHH:MM:SSZ)"
-    ),
-    created_at_end: Optional[datetime] = Query(
-        None, description="Filter by createdAt date (YYYY-MM-DDTHH:MM:SSZ)"
-    ),
-):
-    pass
-
-
-@router.get(
-    "/{org_id}/{project_id}/synaptome-models",
-    summary="Retrieve all synaptome models for a specific project",
-)
-def retrieve_synaptome_models(
-    org_id: str,
-    project_id: str,
-    page_offset: int = 0,
-    page_size: int = 20,
-    created_at_start: Optional[datetime] = Query(
-        None, description="Filter by createdAt date (YYYY-MM-DDTHH:MM:SSZ)"
-    ),
-    created_at_end: Optional[datetime] = Query(
-        None, description="Filter by createdAt date (YYYY-MM-DDTHH:MM:SSZ)"
-    ),
-):
-    pass
-
-
-@router.get(
-    "/{org_id}/{project_id}/synaptome-models/{model_id}",
-    summary="Retrieve single synaptome model in a specific project",
-)
-def retrieve_synaptome_model(
-    model_id: str,
-    org_id: str,
-    project_id: str,
-    page_offset: int = 0,
-    page_size: int = 20,
-    created_at_start: Optional[datetime] = Query(
-        None, description="Filter by createdAt date (YYYY-MM-DDTHH:MM:SSZ)"
-    ),
-    created_at_end: Optional[datetime] = Query(
-        None, description="Filter by createdAt date (YYYY-MM-DDTHH:MM:SSZ)"
-    ),
-):
-    pass
+    token: str = Depends(verify_jwt),
+) -> MEModelResponse | SynaptomeModelResponse:
+    return get_neuron_model_for_project(
+        token=token, org_id=org_id, project_id=project_id, model_self=model_id
+    )
