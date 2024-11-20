1ab69dcadbe0e7a3aaa4cc69cdcc1c0020fe0028
Dinika Saxena <dinikasaxenas@gmail.com>
2024-11-20 15:06:43 +0100

    Move morphology dendogram building to distributed task

---
+++ bluenaas/infrastructure/celery/tasks/build_morphology_dendogram.py
@@ -0,0 +1,32 @@
+import json
+from loguru import logger
+
+from bluenaas.infrastructure.celery import celery_app
+from bluenaas.core.exceptions import MorphologyGenerationError
+
+
+@celery_app.task(
+    bind=True,
+    serializer="json",
+)
+def build_morphology_dendrogram(
+    self,
+    model_self: str,
+    token: str,
+):
+    try:
+        from bluenaas.core.model import model_factory
+
+        logger.debug(f"Started building morphology dendogram for model {model_self}")
+        model = model_factory(
+            model_self=model_self,
+            hyamp=None,
+            bearer_token=token,
+        )
+        morphology_dendrogram = model.CELL.get_dendrogram()
+        return json.dumps(morphology_dendrogram)
+    except Exception as e:
+        logger.exception(f"Exception in celery worker during morphology building {e}")
+        raise MorphologyGenerationError(message=f"Morphology generation failed {e}")
+    finally:
+        logger.debug(f"Completed building morphology dendogram for model {model_self}")
