6e6971bb9e8db5cda6d26ce98211bb65c5fd00bf
Dinika Saxena <dinikasaxenas@gmail.com>
2024-11-20 10:59:39 +0100

    Move morphology building to distributed architecture
    Signed-off-by: Dinika Saxena <dinikasaxenas@gmail.com>

--- bluenaas/services/morphology.py
+++ bluenaas/services/morphology.py
@@ -1,56 +1,20 @@
-import json
 import re
-import signal
-import multiprocessing as mp
-from multiprocessing.synchronize import Event
-from bluenaas.utils.streaming import StreamingResponseWithCleanup, cleanup
+import json
+from typing import Generator
 from loguru import logger
+
+from bluenaas.utils.streaming import (
+    StreamingResponseWithCleanup,
+    cleanup_worker,
+)
 from http import HTTPStatus as status
-from queue import Empty as QueueEmptyException
+from bluenaas.infrastructure.celery.tasks.build_morphology import build_morphology
+
 from bluenaas.core.exceptions import (
     BlueNaasError,
     BlueNaasErrorCode,
     MorphologyGenerationError,
 )
-from bluenaas.core.model import model_factory
-from bluenaas.utils.const import QUEUE_STOP_EVENT
-
-
-def _build_morphology(
-    model_self: str,
-    token: str,
-    queue: mp.Queue,
-    stop_event: Event,
-):
-    def stop_process():
-        stop_event.set()
-
-    signal.signal(signal.SIGTERM, stop_process)
-    signal.signal(signal.SIGINT, stop_process)
-
-    try:
-        model = model_factory(
-            model_self=model_self,
-            hyamp=None,
-            bearer_token=token,
-        )
-        morphology = model.CELL.get_cell_morph()
-        morph_str = json.dumps(morphology)
-        chunks: list[str] = re.findall(r".{1,100000}", morph_str)
-
-        for index, chunk in enumerate(chunks):
-            logger.debug(f"Queueing chunk {index} for morphology...")
-            queue.put(chunk)
-
-        queue.put(QUEUE_STOP_EVENT)
-
-    except Exception as ex:
-        logger.exception(f"Morphology builder error: {ex}")
-        exception = MorphologyGenerationError(ex.__str__())
-        queue.put(exception)
-        queue.put(QUEUE_STOP_EVENT)
-    finally:
-        logger.debug("Morphology builder ended")
 
 
 def get_single_morphology(
@@ -59,61 +23,38 @@ def get_single_morphology(
     req_id: str,
 ):
     try:
-        ctx = mp.get_context("spawn")
-
-        morpho_queue = ctx.Queue()
-        stop_event = ctx.Event()
-
-        process = ctx.Process(
-            target=_build_morphology,
-            args=(
-                model_self,
-                token,
-                morpho_queue,
-                stop_event,
-            ),
-            name=f"morphology_processor:{req_id}",
+        build_morphology_job = build_morphology.apply_async(
+            kwargs={
+                "model_self": model_self,
+                "token": token,
+            }
         )
-        process.daemon = True
-        process.start()
+        logger.debug(f"Started morphology task {build_morphology_job.id}")
+        built_morphology_str = build_morphology_job.get()
 
-        def queue_streamify(
-            que: mp.Queue,
-            stop_event: Event,
-        ):
-            while True:
-                try:
-                    q_result = que.get(timeout=1)
-                except QueueEmptyException:
-                    if process.is_alive():
-                        continue
-                    if not que.empty():
-                        # Checking if queue is empty again to avoid the following race condition:
-                        # t0 - Empty exception is raised from queue.get()
-                        # t1 - Child process writes to queue
-                        # t2 - Child process finishes
-                        # t3 - Queue should be checked again for emptiness to capture the last message
-                        continue
-                    else:
-                        raise Exception("Child process died unexpectedly")
-                if isinstance(q_result, MorphologyGenerationError):
-                    yield f"{json.dumps(
-                        {
-                            "error_code": BlueNaasErrorCode.MORPHOLOGY_GENERATION_ERROR,
-                            "message": "Morphology generation failed",
-                            "details": q_result.__str__(),
-                        }
-                    )}\n"
-                    break
-                if q_result == QUEUE_STOP_EVENT or stop_event.is_set():
-                    break
+        def stream_morphology_chunks() -> Generator[str, None, None]:
+            if isinstance(built_morphology_str, MorphologyGenerationError):
+                yield f"{json.dumps(
+                    {
+                        "error_code": BlueNaasErrorCode.MORPHOLOGY_GENERATION_ERROR,
+                        "message": "Morphology generation failed",
+                        "details": built_morphology_str.__str__(),
+                    }
+                )}\n"
+                return
 
-                yield q_result
+            chunks: list[str] = re.findall(r".{1,100000}", built_morphology_str)
+
+            for index, chunk in enumerate(chunks):
+                logger.debug(f"Queueing chunk {index} for morphology...")
+                yield chunk
 
         return StreamingResponseWithCleanup(
-            queue_streamify(que=morpho_queue, stop_event=stop_event),
+            stream_morphology_chunks(),
             media_type="application/x-ndjson",
-            finalizer=lambda: cleanup(stop_event, process),
+            finalizer=lambda: cleanup_worker(
+                build_morphology_job.id,
+            ),
         )
 
     except Exception as ex:
