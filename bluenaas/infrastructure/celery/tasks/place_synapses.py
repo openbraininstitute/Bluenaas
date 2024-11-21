1c670977627bccd72d42b5b8cf08546601387ea3
Dinika Saxena <dinikasaxenas@gmail.com>
2024-11-21 09:49:07 +0100

    Fix neuron error in synaptome placement

--- bluenaas/infrastructure/celery/tasks/place_synapses.py
+++ bluenaas/infrastructure/celery/tasks/place_synapses.py
@@ -1,9 +1,13 @@
 import json
 from loguru import logger
+import billiard  # type: ignore
+from billiard.queues import Empty as QueueEmptyException  # type: ignore
 
 from bluenaas.infrastructure.celery import celery_app
 from bluenaas.core.exceptions import SynapseGenerationError
 from bluenaas.domains.morphology import SynapsePlacementBody
+
+SYNAPSE_PLACEMENT_TIMEOUT_SECONDS = 5 * 60
 
 
 @celery_app.task(
@@ -16,6 +20,33 @@ def place_synapses(
     token: str,
     synapse_placement_config: str,  # JSON string representing object of type StimulationPlotConfig
 ):
+    queue = billiard.Queue()
+    process = billiard.Process(
+        target=_place_synapses_subprocess,
+        args=(queue, model_self, token, synapse_placement_config),
+    )
+    process.start()
+    try:
+        result = queue.get(timeout=SYNAPSE_PLACEMENT_TIMEOUT_SECONDS)
+        if isinstance(result, Exception):
+            raise result
+
+        return result
+    except QueueEmptyException:
+        raise SynapseGenerationError(
+            f"Did not receive synaptome result in {SYNAPSE_PLACEMENT_TIMEOUT_SECONDS} seconds"
+        )
+    except Exception as e:
+        raise e
+    finally:
+        logger.debug("Cleaning up the worker process")
+        process.join()
+        logger.debug("Cleaning done")
+
+
+def _place_synapses_subprocess(
+    queue: billiard.Queue, model_self: str, token: str, synapse_placement_config: str
+) -> None:
     try:
         from bluenaas.core.model import model_factory
 
@@ -29,9 +60,10 @@ def place_synapses(
         )
         synapses = model.add_synapses(params=placement_config)
 
-        return synapses.model_dump_json()
+        queue.put(synapses.model_dump_json())
     except Exception as e:
         logger.exception(f"Exception in celery worker during placing synapses {e}")
-        raise SynapseGenerationError(message=f"Synaptome placement failed {e}")
+        queue.put(SynapseGenerationError(message=f"Synaptome placement failed {e}"))
     finally:
         logger.debug(f"Completed placing synapses for model {model_self}")
+        return
