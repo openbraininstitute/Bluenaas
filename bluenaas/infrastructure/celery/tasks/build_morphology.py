b927bef1fb47b15e5a3bc678c64a485046ae98fe
Dinika Saxena <dinikasaxenas@gmail.com>
2024-11-21 09:21:27 +0100

    Fix neuron errors in morphology building task

--- bluenaas/infrastructure/celery/tasks/build_morphology.py
+++ bluenaas/infrastructure/celery/tasks/build_morphology.py
@@ -1,8 +1,12 @@
 import json
 from loguru import logger
+import billiard  # type: ignore
+from billiard.queues import Empty as QueueEmptyException  # type: ignore
 
 from bluenaas.infrastructure.celery import celery_app
 from bluenaas.core.exceptions import MorphologyGenerationError
+
+MORPHOLOGY_BUILD_TIMEOUT_SECONDS: int = 5 * 60
 
 
 @celery_app.task(
@@ -14,6 +18,31 @@ def build_morphology(
     model_self: str,
     token: str,
 ):
+    queue = billiard.Queue()
+    process = billiard.Process(
+        target=_build_morphology_subprocess,
+        args=(queue, model_self, token),
+    )
+    process.start()
+    try:
+        result = queue.get(timeout=MORPHOLOGY_BUILD_TIMEOUT_SECONDS)
+        if isinstance(result, Exception):
+            raise result
+
+        return result
+    except QueueEmptyException:
+        raise MorphologyGenerationError(
+            f"Did not receive morphology result in {MORPHOLOGY_BUILD_TIMEOUT_SECONDS} seconds"
+        )
+    except Exception as e:
+        raise e
+    finally:
+        logger.debug("Cleaning up the worker process")
+        process.join()
+        logger.debug("Cleaning done")
+
+
+def _build_morphology_subprocess(queue: billiard.Queue, model_self: str, token: str):
     try:
         from bluenaas.core.model import model_factory
 
@@ -25,9 +54,12 @@ def build_morphology(
         )
         morphology = model.CELL.get_cell_morph()
         morph_str = json.dumps(morphology)
-
-        logger.debug(f"Completed building morphology for model {model_self}")
-        return morph_str
+        queue.put(morph_str)
     except Exception as e:
         logger.exception(f"Exception in celery worker during morphology building {e}")
-        raise MorphologyGenerationError(message=f"Morphology generation failed {e}")
+        queue.put(
+            MorphologyGenerationError(message=f"Morphology generation failed {e}")
+        )
+    finally:
+        logger.debug(f"Finished building morphology for model {model_self}")
+        return
