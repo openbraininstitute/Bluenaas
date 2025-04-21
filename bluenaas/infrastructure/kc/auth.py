8dd2cc75512fdb6db8dd621ed345b77bd039bcc0
Bilal MEDDAH <b_meddah@esi.dz>
2025-04-21 20:50:51 +0200

    make only email, username, sub, and username as required in the token (#13)

--- bluenaas/infrastructure/kc/auth.py
+++ bluenaas/infrastructure/kc/auth.py
@@ -1,14 +1,14 @@
 from http import HTTPStatus as status
-from loguru import logger
-
 from typing import List, Optional
-from pydantic import BaseModel, Field
+
 from fastapi import Depends
 from fastapi.security import (
     HTTPAuthorizationCredentials,
     HTTPBearer,
     OAuth2AuthorizationCodeBearer,
 )
+from loguru import logger
+from pydantic import BaseModel, Field
 
 from bluenaas.config.settings import settings
 from bluenaas.core.exceptions import BlueNaasError, BlueNaasErrorCode
@@ -23,36 +23,49 @@ KC_SUBJECT: str = f"service-account-{settings.KC_CLIENT_ID}"
 class DecodedKeycloakToken(BaseModel):
     # JWT Standard Fields (RFC 7519)
     exp: int = Field(description="Expiration timestamp of the token")
-    iat: int = Field(description="Timestamp when the token was issued")
-    jti: str = Field(description="Unique identifier for the token")
+    iat: Optional[int] = Field(
+        description="Timestamp when the token was issued", default=None
+    )
+    jti: Optional[str] = Field(
+        description="Unique identifier for the token", default=None
+    )
     iss: str = Field(
         description="URL of the authentication server that issued the token"
     )
     sub: str = Field(description="Unique identifier for the user")
-    aud: List[str] | str = Field(description="Intended recipient of the token")
+    aud: Optional[List[str] | str] = Field(
+        description="Intended recipient of the token", default=None
+    )
 
     # OIDC Standard Fields
-    typ: str = Field(description="Token type, usually 'Bearer'")
-    azp: str = Field(
-        description="Authorized party - the client that requested the token"
+    typ: Optional[str] = Field(description="Token type, usually 'Bearer'", default=None)
+    azp: Optional[str] = Field(
+        description="Authorized party - the client that requested the token",
+        default=None,
     )
-    scope: str = Field(description="Space-separated list of granted scopes")
-    email_verified: bool = Field(
-        description="Indicates if the user's email is verified"
+    scope: Optional[str] = Field(
+        description="Space-separated list of granted scopes", default=None
     )
-    name: str = Field(description="Full name of the user")
+    email_verified: Optional[bool] = Field(
+        description="Indicates if the user's email is verified", default=None
+    )
+    name: Optional[str] = Field(description="Full name of the user", default=None)
     preferred_username: str = Field(description="User's preferred username")
-    given_name: str = Field(description="User's first name")
-    family_name: str = Field(description="User's last name")
+    given_name: Optional[str] = Field(description="User's first name", default=None)
+    family_name: Optional[str] = Field(description="User's last name", default=None)
     email: str = Field(description="User's email address")
 
     # Keycloak-Specific Fields
-    auth_time: int = Field(description="Timestamp of the original authentication")
+    auth_time: Optional[int] = Field(
+        description="Timestamp of the original authentication", default=None
+    )
     session_state: Optional[str] = Field(
         description="Keycloak's session identifier", default=None
     )
-    acr: str = Field(description="Authentication Context Class Reference")
-    sid: str = Field(description="Keycloak session ID")
+    acr: Optional[str] = Field(
+        description="Authentication Context Class Reference", default=None
+    )
+    sid: Optional[str] = Field(description="Keycloak session ID", default=None)
     # * Add realm_access and resource_access fields if necessary.
 
 
