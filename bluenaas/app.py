67914cd22169694ec36008b46fdfbe8179e6ebee
Gil Arturo Barrios del Villar <gilarturo.barriosdelvillar@openbraininstitute.org>
2025-05-08 17:04:16 +0200

    entitycore routes

--- bluenaas/app.py
+++ bluenaas/app.py
@@ -13,6 +13,7 @@ from bluenaas.core.exceptions import (
     BlueNaasErrorResponse,
 )
 from bluenaas.routes.morphology import router as morphology_router
+from bluenaas.routes.entitycore.morphology import router as entitycore_morphology_router
 from bluenaas.routes.simulation import router as simulation_router
 from bluenaas.routes.graph_data import router as graph_router
 from bluenaas.routes.synaptome import router as synaptome_router
@@ -86,6 +87,7 @@ async def bluenaas_exception_handler(
 
 
 base_router = APIRouter(prefix=settings.BASE_PATH)
+entitycore_router = APIRouter(prefix=settings.BASE_PATH + "/entitycore")
 
 
 @base_router.get("/")
@@ -106,5 +108,7 @@ base_router.include_router(graph_router)
 base_router.include_router(validation_router)
 base_router.include_router(neuron_model_router)
 
+entitycore_router.include_router(entitycore_morphology_router)
 
 app.include_router(base_router)
+app.include_router(entitycore_router)
---
+++ bluenaas/routes/entitycore/morphology.py
@@ -0,0 +1,34 @@
+from typing import Annotated
+from bluenaas.services.morphology import get_single_morphology
+from bluenaas.services.morphology_dendrogram import get_single_morphology_dendrogram
+from fastapi import APIRouter, Depends, Query, Request
+
+from bluenaas.infrastructure.kc.auth import verify_jwt, Auth
+
+router = APIRouter(prefix="/morphology")
+
+
+@router.get("")
+def retrieve_morphology(
+    request: Request,
+    auth: Annotated[Auth, Depends(verify_jwt)],
+    model_id: str = "",
+):
+    return get_single_morphology(
+        model_id=model_self,
+        token=auth.token,
+        req_id=request.state.request_id,
+    )
+
+
+@router.get("/dendrogram")
+def retrieve_morphology_dendrogram(
+    request: Request,
+    model_id: str = Query(""),
+    auth: Auth = Depends(verify_jwt),
+):
+    return get_single_morphology_dendrogram(
+        model_id=model_id,
+        token=auth.token,
+        req_id=request.state.request_id,
+    )
