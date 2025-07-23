5c4381a1ef334e10abb2de59582ec174dda304bd
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2025-07-23 15:36:55 +0200

    Bump ready marker version (to invalidate current cache)

--- app/constants.py
+++ app/constants.py
@@ -11,4 +11,4 @@ SINGLE_NEURON_MORPHOLOGY_DIR = "morphology"
 LIBNRNMECH_PATH = "x86_64/lib/nrnmech.so"
 
 # TODO revert back to .ready once the cache storage is cleaned
-READY_MARKER_FILE_NAME = ".ready-v1"
+READY_MARKER_FILE_NAME = ".ready-v2"
